9fa247afa3ab34a679b65e7449946bc2e0431686
dodomyg <sharvil.goverdhan@gmail.com>
2024-10-25 16:25:58 +0530

    init

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -113,6 +113,7 @@ const Home = () => {
           <option value="Python">Python</option>
           <option value="Javascript">Javascript</option>
           <option value="Java">Java</option>
+          <option value="Go">Go</option>
           <option value="C">C</option>
           <option value="C++">C++</option>
         </select>
